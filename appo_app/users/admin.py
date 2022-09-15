75cddbb778ab0a140422c16e10b4743f72d91049
Antonio Vilchez Monge <antoniovmonge@gmail.com>
2022-09-15 15:51:48 +0200

    initial push with no-username-cookiecutter

---
+++ appo_app/users/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.utils.translation import gettext_lazy as _
+
+from appo_app.users.forms import CustomUserChangeForm, CustomUserCreationForm
+
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(auth_admin.UserAdmin):
+
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    model = CustomUser
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("name",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ["email", "name", "is_superuser"]
+    search_fields = ["name"]
+    ordering = ("email",)
